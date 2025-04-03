41ad2d7b6934183913d2ef7c218cd6433fe82df6
nizartormane <Nizar.tormanee@gmail.com>
2025-04-03 11:39:46 +0100

    first commit

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,18 +1,37 @@
-import nextJest from 'next/jest.js';
-
-const createJestConfig = nextJest({
-  dir: './',
-});
-
-const customJestConfig = {
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testEnvironment: 'jest-environment-jsdom',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+export default {
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
   },
-  preset: 'ts-jest',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  transform: {}, // Ajout de cette ligne
+  extensionsToTreatAsEsm: [".ts", ".tsx"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
+  testEnvironment: "node", // ou "jsdom" si c'est un projet frontend
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
 
-export default createJestConfig(customJestConfig);
+
+
+
+// import nextJest from 'next/jest.js';
+
+// const createJestConfig = nextJest({
+//   dir: './',
+// });
+
+// const customJestConfig = {
+//   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+//   testEnvironment: 'jest-environment-jsdom',
+//   moduleNameMapper: {
+//     '^@/(.*)$': '<rootDir>/src/$1',
+//   },
+//   preset: 'ts-jest',
+//   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+//   transform: {}, // Ajout de cette ligne
+// };
+
+// export default createJestConfig(customJestConfig);
